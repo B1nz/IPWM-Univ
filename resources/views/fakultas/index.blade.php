92a34f8aa37bf8d79819c28de83f0d6417e23174
B1nz <Goldiansyah@gmail.com>
2020-05-14 15:55:15 +0700

    Modal Fix

--- resources/views/fakultas/index.blade.php
+++ resources/views/fakultas/index.blade.php
@@ -32,31 +32,7 @@
 
             <button type="button" class="btn btn-success" data-toggle="modal" data-target="#importExcel">Import Excel</button>
 
-            <div class="modal fade" id="importExcel" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-              <div class="modal-dialog" role="document">
-                <form method="post" action="fakultas/import" enctype="multipart/form-data">
-                  <div class="modal-content">
-                    <div class="modal-header">
-                      <h5 class="modal-title" id="exampleModalLabel">Import Excel</h5>
-                    </div>
-                    <div class="modal-body">
-        
-                      {{ csrf_field() }}
-        
-                      <label>Pilih file excel</label>
-                      <div class="form-group">
-                        <input type="file" name="file" required="required">
-                      </div>
-        
-                    </div>
-                    <div class="modal-footer">
-                      <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                      <button type="submit" class="btn btn-primary">Import</button>
-                    </div>
-                  </div>
-                </form>
-              </div>
-            </div>
+            
 
           {{-- End Excel import --}}
 
@@ -105,4 +81,31 @@
   </div>
 
 </section>
+
+<div class="modal fade" id="importExcel" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <form method="post" action="fakultas/import" enctype="multipart/form-data">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Import Excel</h5>
+        </div>
+        <div class="modal-body">
+        
+          {{ csrf_field() }}
+        
+          <label>Pilih file excel</label>
+          <div class="form-group">
+            <input type="file" name="file" required="required">
+          </div>
+        
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+          <button type="submit" class="btn btn-primary">Import</button>
+        </div>
+      </div>
+    </form>
+  </div>
+</div>
+
 @endsection()
